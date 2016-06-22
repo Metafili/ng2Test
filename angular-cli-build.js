fb75a2ff1382da3e043e7a074f4fd37663fc185e
edicon <hslee.edicon@gmail.com>
2016-06-22 18:24:46 +0900

    -mapRoot:/ for *.ts generation

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -7,9 +7,9 @@ module.exports = function(defaults) {
     vendorNpmFiles: [
       'systemjs/dist/system-polyfills.js',
       'systemjs/dist/system.src.js',
-      'zone.js/dist/*.+(js|js.map)',
+      'zone.js/dist/**/*.+(js|js.map)',
       'es6-shim/es6-shim.js',
-      'reflect-metadata/*.+(js|js.map)',
+      'reflect-metadata/**/*.+(ts|js|js.map)',
       'rxjs/**/*.+(js|js.map)',
       '@angular/**/*.+(js|js.map)',
       'angularfire2/**/*.+(js|js.map)',
