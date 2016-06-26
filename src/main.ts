86a3f10fd00986f8db96af1e1eb9dcbaceb2d07e
EDICON <hslee.edicon@gmail.com>
2016-06-26 14:04:47 +0900

    -Update firebase3.0 and angularfire beta2

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,13 @@ bootstrap(Ng2TestAppComponent, [
   HTTP_PROVIDERS,
   provide(HAMMER_GESTURE_CONFIG, {useClass: MdGestureConfig}),
   FIREBASE_PROVIDERS,
-  defaultFirebase('https://vivid-torch-3052.firebaseio.com/'),
+  // defaultFirebase('https://vivid-torch-3052.firebaseio.com/'),
+  defaultFirebase({
+    apiKey: "AIzaSyCT1e0MDRmnD_9juAp7VMWCLnJDiTJu7JY",
+    authDomain: "vivid-torch-3052.firebaseapp.com",
+    databaseURL: "https://vivid-torch-3052.firebaseio.com/",
+    storageBucket: "vivid-torch-3052.appspot.com",
+  }),
   firebaseAuthConfig({
     provider: AuthProviders.Password,
     method: AuthMethods.Password,
