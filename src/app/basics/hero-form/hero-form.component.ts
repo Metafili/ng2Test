f619b3dfa4a0aa783de5725e7cc6029cbab78dc3
edicon <hslee.edicon@gmail.com>
2016-07-01 17:35:15 +0900

    Add and Debugging Forms

--- src/app/basics/hero-form/hero-form.component.ts
+++ src/app/basics/hero-form/hero-form.component.ts
@@ -32,6 +32,17 @@ export class HeroFormComponent implements OnInit {
 
   onSubmit() { this.submitted = true; }
 
+  // Reset the form with a new hero AND restore 'pristine' class state
+  // by toggling 'active' flag which causes the form
+  // to be removed/re-added in a tick via NgIf
+  // TODO: Workaround until NgForm has a reset method (#6822)
+  active = true;
+  newHero() {
+    this.model = new Hero(42, '', '');
+    this.active = false;
+    setTimeout(() => this.active = true, 0);
+  }
+
   // TODO: Remove this when we're done
   get diagnostic() { return JSON.stringify(this.model); }
 }
